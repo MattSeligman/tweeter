489b7e90fc9944dc80d09da063ac48854e69867c
MattSeligman <matt@modelitup.com>
2022-03-28 21:01:19 -0700

    Added jQuery test and moved Tweet css to its own file.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,22 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+var characterLimit = 140;
+$(document).ready(function(){
+
+ $('input').on('keydown', ( pressed )=>{
+
+    // bugs to fix 
+    // [] Delete key won't detect in current method
+    // [] HTML limit can be removed.)
+   if(characterLimit === 0){ console.log(pressed); return false}
+   if(characterLimit > 0){
+     characterLimit--;
+
+     $('output.counter').text(characterLimit);
+
+   }
+             
+ });
+
+});
