538b7a54681ef626d4d3b2dffd3ed4e4d8e0e9e8
MattSeligman <matt@modelitup.com>
2022-03-31 22:44:51 -0700

    Added media query for Desktop and applied eslint

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,78 +8,80 @@
 
 $(() => {
 
-    const tweetsContainer = 'body > main';
-    const tweetForm = 'section.new-tweet form';
-    const tweetButton = 'section.new-tweet form button';
-    const textAreaInput = 'textarea#tweet-text';
-    let tweets = null;
+  const tweetsContainer = 'body > main';
+  const tweetForm = 'section.new-tweet form';
+  const tweetButton = 'section.new-tweet form button';
+  const textAreaInput = 'textarea#tweet-text';
+  let tweets = null;
     
 
-    var p = $( textAreaInput )
-    var position = p.position();
+  let p = $(textAreaInput);
+  let position = p.position();
 
-    // let test = $( "p" ).last().text( "left: " + position.left + ", top: " + position.top );
-    console.log(`position`, position)
+  // let test = $( "p" ).last().text( "left: " + position.left + ", top: " + position.top );
+  console.log(`position`, position);
 
 
-    /**
+  /**
      * Loads tweets from the route /tweets/ and pulls the order in reverse.
      * * `onPost` only renders the current tweet.
-     * @param {string} onPost 
+     * @param {string} onPost
      */
-    const loadtweets = (onPost)=>{
+  const loadtweets = (onPost)=>{
         
-        $.getJSON( "/tweets/", ( tweetData ) => {
-            tweets = tweetData.reverse();
+    $.getJSON("/tweets/", (tweetData) => {
+      tweets = tweetData.reverse();
 
-            if(onPost === 'currentTweet'){
-                console.log("Triggered")
-                const latestEntry = tweetData[0];
-                const newTweetSection = 'body > main > section.new-tweet + section'
-                return $(newTweetSection).prepend( createTweetElement( latestEntry ) );
-            }
+      if (onPost === 'currentTweet') {
+        console.log("Triggered");
+        const latestEntry = tweetData[0];
+        const newTweetSection = 'body > main > section.new-tweet + section';
+        return $(newTweetSection).prepend(createTweetElement(latestEntry));
+      }
 
-            return renderTweets(tweets);
+      return renderTweets(tweets);
 
-        });
-    }
+    });
+  };
     
-    loadtweets();
+  loadtweets();
 
-    /**
+  /**
      * Protects from XSS Attacks
      * * Create's temporary div elment
      * * Create's a textNode from the input and adds it within the temporary div.
      * * Returns only the innerHTML of the text (pure text format of any symbols)
-     * @param {String} input 
+     * @param {String} input
      * @returns sanitizedText
      */
-    const escape = (input)=>{
-        let temporaryContainer = document.createElement("div");
-        temporaryContainer.appendChild(document.createTextNode(input));
-        return temporaryContainer.innerHTML;
-      };
+  const escape = (input)=>{
+    let temporaryContainer = document.createElement("div");
+    temporaryContainer.appendChild(document.createTextNode(input));
+    return temporaryContainer.innerHTML;
+  };
 
-    /**
+  /**
      * Create's the Tweet Element for append/prepending
-     * * Takes in input data & reformats correctly 
+     * * Takes in input data & reformats correctly
      * * Sanitizes input to prevent XSS with the `escape(string)` function.
-     * @param {object} tweetData 
+     * @param {object} tweetData
      * @returns a formatted element for Tweets
      */
-    const createTweetElement = (tweetData)=>{
+  const createTweetElement = (tweetData)=>{
 
-        const username = escape(tweetData.user.name);
-        const handle = escape(tweetData.user.handle);
-        const tweetMessage = escape(tweetData.content.text);
-        const createdAt = escape(tweetData.created_at);
-        const timestamp = timeago.format(createdAt);
-        let avatar = escape(tweetData.user.avatars)
-        const defaultAvatar = 'icon-user-01';
+    const username = escape(tweetData.user.name);
+    const handle = escape(tweetData.user.handle);
+    const tweetMessage = escape(tweetData.content.text);
+    const createdAt = escape(tweetData.created_at);
+    const timestamp = timeago.format(createdAt);
+    let avatar = escape(tweetData.user.avatars);
+    const defaultAvatar = 'icon-user-01';
         
-        if(!avatar){ avatar = defaultAvatar }
+    if (!avatar) {
+      avatar = defaultAvatar;
+    }
 
-        const tweet = 
+    const tweet =
         `<section>
             <article class="tweet">
             <header>
@@ -100,113 +102,122 @@ $(() => {
             </article>
         </section>`;
         
-        return tweet;
-    }
+    return tweet;
+  };
 
-    /**
+  /**
      * Loops through existing tweets & appends to the Tweets Container
      * * Uses the `createTweetElement(object)` function
-     * @param {object} tweets 
-     */ 
-    const renderTweets = (tweets)=>{
+     * @param {object} tweets
+     */
+  const renderTweets = (tweets)=>{
 
-        for( let tweet of tweets){
-            $(tweetsContainer).append( createTweetElement(tweet) );
-        }   
+    for (let tweet of tweets) {
+      $(tweetsContainer).append(createTweetElement(tweet));
     }
+  };
 
-    /**
+  /**
      * Focus's the user on the Tweet input field
      * * [ ] Goal - add mini background animation if time permits
      */
-    const focusOnInput = ()=>{ 
-        $(textAreaInput).focus(); 
-    }
+  const focusOnInput = ()=>{
+    $(textAreaInput).focus();
+  };
        
 
-    // force HTML5 validation (Even though they can remove it manually)
-    $(textAreaInput).prop('required',true);
+  // force HTML5 validation (Even though they can remove it manually)
+  $(textAreaInput).prop('required',true);
 
-    /* On Tweet */
-    $(tweetForm).on('submit', (event)=>{
-        event.preventDefault();
+  /* On Tweet */
+  $(tweetForm).on('submit', (event)=>{
+    event.preventDefault();
 
-        // grab the int value of the text counter
-        const counter = parseInt( $('section.new-tweet output.counter').text() );
+    // grab the int value of the text counter
+    const counter = parseInt($('section.new-tweet output.counter').text());
 
-        const submission = $(textAreaInput).val();
-        const tooManyCharacters = (counter < 0);
-        const noCharacters = submission === '' || submission === null;
-        const warning = $('.warning');
+    const submission = $(textAreaInput).val();
+    const tooManyCharacters = (counter < 0);
+    const noCharacters = submission === '' || submission === null;
+    const warning = $('.warning');
         
-        if(tooManyCharacters || noCharacters){
-            $(warning).removeClass( 'hidden' );
+    if (tooManyCharacters || noCharacters) {
+      $(warning).removeClass('hidden');
 
-            /* slideDown method available if required but I found it sluggish */
-            // if ( $( warning ).is( ":hidden" ) ) {
-            //     $( warning ).slideDown( "slow" );
-            //   } else {
-            //     $( warning ).hide();
-            //   }
+      /* slideDown method available if required but I found it sluggish */
+      // if ( $( warning ).is( ":hidden" ) ) {
+      //     $( warning ).slideDown( "slow" );
+      //   } else {
+      //     $( warning ).hide();
+      //   }
               
-            if(tooManyCharacters){
-                focusOnInput();
+      if (tooManyCharacters) {
+        focusOnInput();
                 
-                return $(warning).text( `Woops! We've used too many characters.` );
-            }
+        return $(warning).text(`Woops! We've used too many characters.`);
+      }
 
-            focusOnInput();
-            return $(warning).text( `Woops! We've forgotten our tweet!` );
-        }
+      focusOnInput();
+      return $(warning).text(`Woops! We've forgotten our tweet!`);
+    }
 
-        $(tweetButton).text('Posting Tweeting');
+    $(tweetButton).text('Posting Tweeting');
 
-        $.post({
-            type: "POST",
-            url: "/tweets/",
-            data:  $(tweetForm).serialize(),
-            success: ()=>{
-                $(warning).addClass( 'hidden' );
-                $(textAreaInput).val('');
-                $(tweetButton).text('Tweet'); 
+    $.post({
+      type: "POST",
+      url: "/tweets/",
+      data:  $(tweetForm).serialize(),
+      success: ()=>{
+        $(warning).addClass('hidden');
+        $(textAreaInput).val('');
+        $(tweetButton).text('Tweet');
                 
-            }
-        }).done(()=>{
+      }
+    }).done(()=>{
 
-            loadtweets('currentTweet');  
-        });  
-
+      loadtweets('currentTweet');
     });
 
-    $( "span.navCTA" ).click(()=>{
-        
-        let scrollLocation = $(window).scrollTop();
+  });
 
-        if (scrollLocation === 400){
-            return $(window).scrollTop('680')
-        }
+  $("span.navCTA").click(()=>{
         
-        $(window).scrollTop('400');
-    });
+    let scrollLocation = $(window).scrollTop();
+    const inputLocation = ($('textarea#tweet-text').offset().top - 200);
+    const postLocation = ($('body > main > section:nth-child(3)').offset().top);
+        
+    if (scrollLocation <= inputLocation) {
 
-    // Scroll to to Top Function
-    // [ ] Note: Requested to put in Composer.js file 
-    // (not sure why single function new file)?
-    $( window ).scroll(()=>{
+      return $(window).scrollTop(postLocation);
+    }
         
-        let scrollLocation = $(window).scrollTop();
-        if(scrollLocation > 400){
+    $(window).scrollTop(inputLocation);
+  });
+
+  // Scroll to to Top Function
+  // [ ] Note: Requested to put in Composer.js file
+  // (not sure why single function new file)?
+  $(window).scroll(()=>{
+        
+    let scrollLocation = $(window).scrollTop();
+    if (scrollLocation > 400) {
             
-            $('.scrollToTop').css('display', 'hidden');
-            return $('.scrollToTop').show();
-        }
+      $('.scrollToTop').css('display', 'hidden');
+            
+      $('nav').css('background-color', 'transparent');
+      $('span.navCTA').hide();
 
-        $('.scrollToTop').hide();
-    });
+      return $('.scrollToTop').show();
+    }
 
-    $( '.scrollToTop' ).click(()=>{
-        $(window).scrollTop('0');
-    });
+    $('nav').css('background-color', '#4056A1');
+    $('span.navCTA').show();
+    $('.scrollToTop').hide();
+  });
+
+  $('.scrollToTop').click(()=>{
+    $(window).scrollTop('0');
+  });
 
 
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,49 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    const characterLimit = 140;
-    let inputLength = 1;
+  const characterLimit = 140;
+  let inputLength = 1;
     
-    const checkLength = ()=>{
-        inputLength = $('textarea#tweet-text', this ).val().length;
-        return inputLength;
+  const checkLength = ()=>{
+    inputLength = $('textarea#tweet-text', this).val().length;
+    return inputLength;
+  };
+
+  const limitControl = ()=>{
+    if (characterLimit - checkLength() < 0) {
+      outputText.addClass('characterLimit');
+      $('.warning').removeClass('hidden');
+      $('.warning').text(`Woops! We've used too many characters.`);
+
+    } else {
+      $('.warning').addClass('hidden');
+      outputText.removeClass('characterLimit');
+    }
+  };
+    
+  const outputText = $('textarea#tweet-text', this).next().children('output');
+
+  $('textarea#tweet-text').on('keydown', (letter)=>{
+    inputLength++;
+        
+    limitControl();
+        
+    outputText.text(characterLimit - checkLength());
+
+    // enter
+    // stretch goal [ ] trigger Post with enter
+    if (letter.keyCode == '13') {
+      console.log("Enter");
+      return false;
     }
 
-    const limitControl = ()=>{
-        if (characterLimit - checkLength() < 0 ){
-            outputText.addClass('characterLimit');
-            $('.warning').removeClass( 'hidden' );
-            $('.warning').text( `Woops! We've used too many characters.` );
-
-        } else {
-            $('.warning').addClass( 'hidden' );
-            outputText.removeClass('characterLimit');
-        }
-    }
-    
-    const outputText = $('textarea#tweet-text', this ).next().children('output');
-
-    $('textarea#tweet-text').on('keydown', ( letter )=>{
-        inputLength++
-        
-        limitControl();
-        
-        outputText.text( characterLimit - checkLength() )        
-
-        // enter 
-        // stretch goal [ ] trigger Post with enter
-        if(letter.keyCode == '13'){
-            console.log("Enter");
-            return false;
-        }
-
-    });
+  });
  
-    $('textarea#tweet-text').on('keyup', ( letter )=>{  
-        outputText.text( characterLimit - checkLength() );
-        limitControl();
+  $('textarea#tweet-text').on('keyup', (letter)=>{
+    outputText.text(characterLimit - checkLength());
+    limitControl();
         
 
-        console.log( characterLimit - checkLength() ); 
-    });
+    console.log(characterLimit - checkLength());
+  });
  
 });
